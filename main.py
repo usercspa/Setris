1037a411602ff6a35059ca0a8399cbdae84b9b33
usercspa <spacchbk@gmail.com>
2021-01-09 08:22:52 +0000

    add length

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import random
 pygame.init()
 
 # game window
-dis_width = 800
-dis_height  = 600
+dis_width = 600
+dis_height  = 400
 dis = pygame.display.set_mode((dis_width, dis_width))
 pygame.display.set_caption('Setris')
 
@@ -17,6 +17,8 @@ blue = (0,0,255)
 white = (255, 255, 255)
 black = (0, 0, 0)
 red = (255, 0, 0)
+yellow = (255, 255, 102)
+green = (0, 255, 0)
 
 game_over = False
 
@@ -24,8 +26,11 @@ game_over = False
 x1 = dis_width/2
 y1 = dis_height/2
  
-snake_speed=30
+snake_speed = 15
 snake_block = 10
+
+font_style = pygame.font.SysFont("bahnschrift", 25)
+score_font = pygame.font.SysFont("comicsansms", 35)
  
 x1_change = 0
 y1_change = 0
@@ -33,13 +38,15 @@ y1_change = 0
 clock = pygame.time.Clock()
 
  
-font_style = pygame.font.SysFont(None, 20)
+def my_snake(snake_block, snake_list):
+    for x in snake_list:
+        pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
  
 def message(msg, color):
     mesg = font_style.render(msg, True, color)
-    dis.blit(mesg, [dis_width/3, dis_height/3])
+    dis.blit(mesg, [dis_width / 6, dis_height / 3])
 
-def gameLoop():  # creating a function
+def gameLoop():
     game_over = False
     game_close = False
  
@@ -49,14 +56,18 @@ def gameLoop():  # creating a function
     x1_change = 0
     y1_change = 0
  
+    snake_List = []
+    Length_of_snake = 1
+ 
     foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-    foody = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+    foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
  
     while not game_over:
  
         while game_close == True:
-            dis.fill(white)
-            message("Gameover! Press Q-Quit or C-Play Again", red)
+            dis.fill(blue)
+            message("Game over! Press C to Play Again or Q to Quit", red)
+ 
             pygame.display.update()
  
             for event in pygame.event.get():
@@ -86,16 +97,31 @@ def gameLoop():  # creating a function
  
         if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
             game_close = True
- 
         x1 += x1_change
         y1 += y1_change
-        dis.fill(white)
-        pygame.draw.rect(dis, blue, [foodx, foody, snake_block, snake_block])
-        pygame.draw.rect(dis, black, [x1, y1, snake_block, snake_block])
+        dis.fill(blue)
+        pygame.draw.rect(dis, green, [foodx, foody, snake_block, snake_block])
+        snake_Head = []
+        snake_Head.append(x1)
+        snake_Head.append(y1)
+        snake_List.append(snake_Head)
+        if len(snake_List) > Length_of_snake:
+            del snake_List[0]
+ 
+        for x in snake_List[:-1]:
+            if x == snake_Head:
+                game_close = True
+ 
+        my_snake(snake_block, snake_List)
+ 
+ 
         pygame.display.update()
  
         if x1 == foodx and y1 == foody:
-            snake_block + 10
+            foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+            foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+            Length_of_snake += 1
+ 
         clock.tick(snake_speed)
  
     pygame.quit()
@@ -103,4 +129,3 @@ def gameLoop():  # creating a function
  
  
 gameLoop()
-  
