7f0f7c323a14473f5e2987e48a479d70be346c53
usercspa <spacchbk@gmail.com>
2021-01-09 06:51:35 +0000

    initial code

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 # Importing packages
 import pygame
 
+#Initializes all of the imported Pygame modules
+pygame.init()
 
+# game window
+dis = pygame.display.set_mode((400, 300))
+pygame.display.update()
+pygame.display.set_caption('Setris')
+game_over = False
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+
+pygame.quit()
+quit()
